c3f52198eaa29d2cffa77e88aa20a5aefb61562e
osdal <osdal67@gmail.com>
2018-09-07 02:27:57 +0300

    Настраиваю webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 let path = require('path'); // Чтобы указать абсолютный путь
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+
 let conf = {
-	entry: './js/bundle/script.js', // Откуда
+	entry: './/js/source/parts/', // Откуда
 	output: { // Куда
-		path : path.resolve(__dirname, './dist-js/'), // В какую папку
+		path : path.resolve(__dirname, './js/dist/'), // В какую папку
 		filename: 'bundler.js', // Как назвать файл
 		publicPath: 'dist-js/' // За кем следить
 	}, // Указываем пути к файлам
