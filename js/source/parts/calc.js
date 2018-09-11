c0e04262b218dba8063f5474a3c5370224f82f95
osdal <osdal67@gmail.com>
2018-09-11 16:53:09 +0300

    Делаю третье задание. Калькулятор. Дошел до обїекта

--- js/source/parts/calc.js
+++ js/source/parts/calc.js
@@ -1,9 +1,29 @@
-﻿let sectionGlazzing = document.getElementsByTagName('section')[0],
+﻿let dataForms = {
+				shape: null,
+				width_window: null,
+				height_window: null
+
+				},
+	sectionGlazzing = document.getElementsByTagName('section')[0],
 	popup_calc = document.querySelector('.popup_calc'),
 	popup_calc_content =document.querySelector('.popup_calc_content'),
 	calcButtons = document.getElementsByClassName('glazing_price_btn'),
 	balcon_icons = document.querySelector('.balcon_icons'),
-	balcon_icons_img = document.querySelectorAll('.balcon_icons img');
+	balcon_icons_img = document.querySelectorAll('.balcon_icons img'),
+	big_img = document.querySelectorAll('.big_img img'),
+	width = document.getElementById('width'),
+	height = document.getElementById('height'),
+	popup_calc_button = document.querySelector('.popup_calc_button'),
+	popup_calc_profile = document.querySelector('.popup_calc_profile'),
+	inputs_calc = document.querySelectorAll('.popup_calc_profile label input'),
+	popup_calc_profile_content = document.getElementsByClassName('popup_calc_profile_content')[0],
+	cold = document.getElementById('cold'),
+	warm = document.getElementById('warm'),
+	popup_calc_profile_button = document.getElementsByClassName('popup_calc_profile_button')[0],
+	popup_calc_end = document.getElementsByClassName('popup_calc_end')[0];
+	console.log(cold);
+	console.log(warm);
+	console.log(popup_calc_profile_button);
 	
 
 
@@ -15,7 +35,11 @@ sectionGlazzing.addEventListener('click', function (event) {
 	}
 });
 
-
+	function reduceBalconImg (a) {
+		if (balcon_icons_img[a].classList.contains('big_img')) {
+			balcon_icons_img[a].classList.remove('big_img');
+		}
+	}
 
 
 	function enlargeBalconImg (b) {
@@ -26,47 +50,82 @@ sectionGlazzing.addEventListener('click', function (event) {
 		
 	}
 
+	function showBigImg (c) {
+		big_img[c].classList.add('show');
+		big_img[c].classList.remove('hide');
+	}
+
+	function hideBigImg (d) {
+		big_img[d].classList.remove('show');
+		big_img[d].classList.add('hide');
+	}
+// Выбор формы балкона и его размеров
 	balcon_icons.addEventListener('click', function (event) {
 			let target = event.target;
 				
 				for (var ii = 0; ii < balcon_icons_img.length; ii++) {
 					if (target == balcon_icons_img[ii]) {
 						enlargeBalconImg (ii);
-						break;
-						
+						dataForms.shape = ii;
+						break;						
 					}
 				}
-				if (ii == 0) {
-					for (let i = 1; i = balcon_icons_img.length; i++) {
-						if (balcon_icons_img[i].classList.contains('big_img')) {
-							balcon_icons_img[i].classList.remove('big_img');
-						}
-					}
-				} else if (ii > 0 && ii < balcon_icons_img.length) {
-					for (let i = ii + 1; i = balcon_icons_img.length; i++) {
-					 	balcon_icons_img[i].classList.remove('big_img');
-					 } 
-					for (let i = 0; i < ii; i++) {
-					 	balcon_icons_img[i].classList.remove('big_img');
-					 }
-				} else if (ii == balcon_icons_img.length) {
-					for (let i = 0; i < balcon_icons_img.length - 1; i++) {
-					 	balcon_icons_img[i].classList.remove('big_img');
-					 }
+				
+				for (let i = (ii + 1); i < balcon_icons_img.length; i++) {
+					reduceBalconImg (i);
 				}
-				
-				
-				
+
+				for (let i = 0; i < ii; i++) {
+					reduceBalconImg (i);
+				}
+
+			showBigImg (ii);
+
+				for (let i = (ii + 1); i < balcon_icons_img.length; i++) {
+					hideBigImg (i);
+				}
+
+				for (let i = 0; i < ii; i++) {
+					hideBigImg (i);
+				}
 				
 	});
 
+
+width.addEventListener('input', function(input) {
+	  			return this.value = this.value.match(/^\d+$/);
+	  				   this.value = dataForms.width;
+	  			
+	  		});
+
+height.addEventListener('input', function(input) {
+	  			return this.value = this.value.match(/^\d+$/);
+	  		});
+console.log(dataForms.width);
+
+popup_calc_button.addEventListener('click', function () {
+	popup_calc.style.display = 'none';
+	popup_calc_profile.classList.remove('popup_calc_profile');
+	popup_calc_profile.classList.add('popup_calc_profile_show');
+});
+
+popup_calc_profile_content.addEventListener('click', function (event) {
+	if (event.target == cold) {
+		cold.classList.add('clicked_cold');
+		warm.classList.remove('clicked_warm');
+	} else if (event.target == warm) {
+		cold.classList.remove('clicked_cold');
+		warm.classList.add('clicked_warm');
+	}
+});
+
+popup_calc_profile_button.addEventListener('click', function () {
+	popup_calc_profile.classList.remove('popup_calc_profile_show');
+	popup_calc_profile.classList.add('popup_calc_profile');
+	popup_calc_end.classList.remove('popup_calc_end');
+	popup_calc_end.classList.add('popup_calc_end_show');
+})
 	
 
-/*balcon_icons.addEventListener('click', function (event) {
-	for (let i = 0; i < balcon_icons_img.length; i++) {
-				if (event.target == balcon_icons_img[i]) {
-					balcon_icons_img[i].classList.add('big_img');
-				}
-	}
-});*/
+
 
