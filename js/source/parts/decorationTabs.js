09611abbfad80401fe7f289f0374fc0b23e5ab5e
osdal <osdal67@gmail.com>
2018-09-12 23:51:01 +0300

    На проверку отделка табы

---
+++ js/source/parts/decorationTabs.js
@@ -0,0 +1,38 @@
+﻿function decorationTabs () {
+	
+}
+
+let decorationTabContainer = document.querySelector('.decoration_slider'),
+	decorationTabLink = document.querySelectorAll('.decoration_tab a'),
+	decorationTab = document.querySelectorAll('.decoration_tab'),
+	decorationTabContent = [
+							document.querySelector('.internal'),
+							document.querySelector('.external'),
+							document.querySelector('.rising'),
+							document.querySelector('.roof')
+							];
+
+console.log(decorationTabContent);
+
+decorationTabContainer.addEventListener('click', function(event) {
+	for (let i = 0; i < decorationTab.length; i++) {
+		if (event.target == decorationTabLink[i]) {
+			decorationTab[i].classList.add('after_click');
+			decorationTabContent[i].classList.add('show');
+			for (let k = i + 1; k < decorationTab.length; k++) {
+				decorationTab[k].classList.remove('after_click');
+				decorationTabContent[k].classList.remove('show');
+				decorationTabContent[k].classList.add('hide');
+			}
+			for (let k = 0; k < i; k++) {
+				decorationTab[k].classList.remove('after_click');
+				decorationTabContent[k].classList.remove('show');
+				decorationTabContent[k].classList.add('hide');
+			}
+		}
+	}
+});
+
+
+
+// module.exports = decorationTabs;
