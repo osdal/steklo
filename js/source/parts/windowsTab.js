50b93e2ba1df47b7eb11dc032308b5fb9f2f6431
osdal <osdal67@gmail.com>
2018-09-12 17:00:03 +0300

    Делаю табы окон

---
+++ js/source/parts/windowsTab.js
@@ -0,0 +1,35 @@
+﻿function wimdowsTab () {
+	
+}
+
+let windowTabsHeaderContainer = document.querySelector('.glazing_slider'),
+	windowTabsHeader = document.querySelectorAll('.glazing_slider a'),
+	windowTabsTree = document.querySelector('.row.tree'),
+	windowTabsAluminum = document.querySelector('.row.aluminum'),
+	windowTabsPlastic = document.querySelector('.row.plastic'),
+	windowTabsFrench = document.querySelector('.row.french'),
+	windowTabsRise = document.querySelector('.row.rise');
+
+console.log(windowTabsHeader);
+
+
+
+windowTabsHeaderContainer.addEventListener('click', function (event) {
+	for (let i = 0; i < windowTabsHeader.length; i++) {
+		if (event.target == windowTabsHeader[i]) {
+			windowTabsHeader[i].classList.add('active');
+			for (let k = i; k < windowTabsHeader.length; k++) {
+				
+				windowTabsHeader[k].classList.remove('active');
+			}
+			
+		}
+	}
+});
+
+
+
+
+
+
+// module.exports = forms;
