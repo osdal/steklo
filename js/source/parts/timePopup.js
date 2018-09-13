33c41d24a836199924d1c24ba2f7fac0056f6977
osdal <osdal67@gmail.com>
2018-09-13 15:42:42 +0300

    Попап по времени на проверку

---
+++ js/source/parts/timePopup.js
@@ -0,0 +1,22 @@
+﻿function timePopup () {
+	
+}
+
+let delay_popup = 3000,
+	popup = document.getElementsByClassName('popup')[0],
+	popup_close_popup = document.querySelector('.popup .popup_close');
+
+	setTimeout("popup.style.display='block'", delay_popup);
+
+    popup_close_popup.addEventListener('click', function(event) {
+    	popup.style.display = 'none';
+    });
+
+    popup.addEventListener('click', function(event) {
+    	if (event.target == popup) {
+    		popup.style.display = 'none';
+    	}
+    });
+
+
+// module.exports = modals;
