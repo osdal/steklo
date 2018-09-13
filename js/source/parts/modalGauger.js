f1e41f42a4e71b6c0efda6ed75aa3abe203a200c
osdal <osdal67@gmail.com>
2018-09-13 20:13:36 +0300

    На проверку модальные окна

--- js/source/parts/modalGauger.js
+++ js/source/parts/modalGauger.js
@@ -7,6 +7,7 @@ function modalGauger () {
 
 let popup_engineer_btn = document.querySelector('.popup_engineer_btn'),
 			popup_engineer = document.querySelector('.popup_engineer'),
+			requestCall = document.querySelector('.contact_us_wrap'),
 			popup_close = document.getElementsByClassName('popup_close')[1];
 
 			
@@ -14,6 +15,11 @@ let popup_engineer_btn = document.querySelector('.popup_engineer_btn'),
 			popup_engineer_btn.addEventListener('click', function() {
 				popup_engineer.style.display = 'block';
 
+
+			});
+
+			requestCall.addEventListener('click', function() {
+				popup_engineer.style.display = 'block';
 
 			});
 
